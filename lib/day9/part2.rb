74629ef8b373d68b22f987b4c1b6612c3916eb20
Andrew Smith <andrewts129@gmail.com>
2023-12-24 13:02:37 -0600

    Day 9, Part 2

---
+++ lib/day9/part2.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Day9Part2
+  def self.solution(lines)
+    _histories(lines).map(&:prediction).sum
+  end
+
+  private
+
+  History = Struct.new(:readings, :keyword_init => true) do
+    def self.parse(line)
+      new(:readings => line.split.map(&:to_i))
+    end
+
+    def prediction
+      deltas = readings.each_cons(2).map { |a, b| b - a}
+      
+      if deltas.uniq.length == 1
+        readings.first - deltas.first
+      else
+        readings.first - History.new(:readings => deltas).prediction
+      end
+    end
+  end
+
+  def self._histories(lines)
+    lines.map do |line|
+      History.parse(line.strip)
+    end
+  end
+end
