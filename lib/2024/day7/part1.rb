d54a47e90911bcf88a52f76fe6c7df92f9034b37
Andrew Smith <andrewts129@gmail.com>
2024-12-08 15:45:58 -0600

    Day 7, part 1

---
+++ lib/2024/day7/part1.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Day7Part1
+  class Equation
+    attr_reader :result
+
+    def initialize(result, values)
+      @result = result 
+      @values = values
+    end
+
+    def self.parse(line)
+      left, right = line.split(":")
+
+      new(
+        left.to_i,
+        right.strip.split(" ").map(&:to_i)
+      )
+    end
+
+    def can_be_valid?
+      return @values.first == @result ? true : false if @values.length == 1
+
+      next_value = @values.first
+      return false if next_value > @result
+
+      add_child_values = @values.drop(1)
+      add_child_values[0] += next_value
+
+      mult_child_values = @values.drop(1)
+      mult_child_values[0] *= next_value
+
+      children = [add_child_values, mult_child_values].map { |v| self.class.new(@result, v) }
+      children.any?(&:can_be_valid?)
+    end
+  end
+
+  def self.solution(lines)
+    lines.map { |l| Equation.parse(l) }.select(&:can_be_valid?).sum(&:result)
+  end
+end
---
+++ spec/2024/day7/part1_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "2024/day7/part1"
+
+describe Day7Part1 do
+  describe ".solution" do
+    subject(:result) { described_class.solution(input) }
+
+    context "on the sample input" do
+      let(:input) { <<~TEXT.split("\n") }
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+      TEXT
+
+      it "returns the correct value" do
+        expect(result).to eq(3749)
+      end
+    end
+
+    context "on the real input" do
+      let(:input) { File.readlines("spec/2024/day7/input.txt") }
+
+      it "returns the correct value" do
+        expect(result).to eq(5702958180383)
+      end
+    end
+  end
+end
